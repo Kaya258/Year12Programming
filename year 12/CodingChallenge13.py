f8b4269fdc06e27bd5c68e4fde559cb1c5011ae8
Kaya258 <19thompsonk@centralfoundationboys.co.uk>
2024-09-26 13:54:48 +0000

    today

--- year 12/CodingChallenge13.py
+++ year 12/CodingChallenge13.py
@@ -1,29 +1,26 @@
-def account(user_ID, student_pin):          #none function
-    attempts = 0
-    while attempts < 3:
-        input_user_ID = input("Please enter your user ID: ")
-        if len(input_user_ID) == 10:
-            print("Username is correct")
-        break
-    while attempts
-        
-
+def accounts(stored_userID, stored_pin):
+    attempts = 3
+    while attempts > 0:
+        input_stored_userID = input("Please enter your user ID: ")
+        if input_stored_userID == 10:
+            print("Correct user ID: ")
+        else:
+            attempts -= 1
+            if attempts > 0:
+                print(f"You have {attempts} attempts left. ")
+            else:
+                print("Incorrect, please try again")
+        attempts = 0
+        max_attempts = 1
+        while attempts < max_attempts:
+            input_stored_pin = int(input("Pleas enter your 4 digit pin. "))
+            if input_stored_pin == stored_pin:
+                print("Correct. ")
+            return
             
-user_ID = "KayaThomps"
-student_pin = "1234"
-
-account(user_ID, student_pin)
-
+            else:
+                print("Incorrect pin")
+            
 
 
 
-
-
-
-
-
-
-
-
-
-#
